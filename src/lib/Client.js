036144bcfa99e200a7b83a3289ef4d6374660824
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-09-25 20:46:01 +0530

    fix: schema import block

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -494,7 +494,7 @@ class KlasaClient extends Discord.Client {
 
 module.exports = KlasaClient;
 
-const { Schema } = require('@klasa/settings-gateway/dist/index');
+const { Schema } = require('@klasa/settings-gateway/dist/lib/schema/Schema');
 
 /**
  * The plugin symbol to be used in external packages
