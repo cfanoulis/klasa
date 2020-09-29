e10154d898652735630209fc1e591d1bb8cf1102
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2020-09-29 18:34:20 +0530

    fix: new events for new sg

--- src/events/coreSettingsUpdate.js
+++ src/events/coreSettingsUpdate.js
@@ -4,17 +4,18 @@ const gateways = ['users', 'clientStorage'];
 module.exports = class extends Event {
 
 	constructor(...args) {
-		super(...args, { event: 'settingsUpdateEntry' });
+		super(...args, { event: 'settingsUpdate' });
 	}
 
-	run(settings) {
-		if (gateways.includes(settings.gateway.type)) {
+	run(settings, updateObject) {
+		if (gateways.includes(settings.gateway.name)) {
 			this.client.shard.broadcastEval(`
 				if (String(this.options.shards) !== '${this.client.options.shards}') {
-					const entry = this.gateways.${settings.gateway.type}.get('${settings.id}');
+					const entry = this.gateways.get('${settings.gateway.name}').get('${settings.id}');
 					if (entry) {
-						entry._patch(${JSON.stringify(settings)});
-						entry._existsInDB = true;
+						entry._patch(${JSON.stringify(updateObject)});
+						entry.existenceStatus = 1;
+						this.emit('settingsSync', entry);
 					}
 				}
 			`);
